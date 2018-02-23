d7144a34829ea6a9141015163983f13108b1cb98
Samantha Salley <samanthasalley@gmail.com>
2018-02-23 15:05:22 -0800

    create server for server-side ejs rendering

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.get('/blog', (req, res) => {
+  res.render('blog');
+});
+
+app.get('/*', (req, res) => {
+  res.render('home');
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
